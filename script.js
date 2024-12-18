1a67810099bd329dd4ca562aa9b914a2fd46bb01
Pranjal Gupta <109664273+72pranjal@users.noreply.github.com>
2024-12-18 09:32:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 function highlight() {
     //Write your code here
-
+ let allStrong = document.querySelectorAll('strong')
+            allStrong.forEach(item=> {
+                item.className = 'add-green'
+            })
 
 }
 
 
 function return_normal() {
     //Write your code here
-
+ let allStrong = document.querySelectorAll('strong')
+            allStrong.forEach(item=> {
+                item.className = 'add-black'
+            })
     
 }
